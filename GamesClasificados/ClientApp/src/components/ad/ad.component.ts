db42ff81cd83ac42fc2f1050610aa7336ef72ee3
Hector Olan <olanhector@gmail.com>
2019-06-01 14:21:51 -0400

    Adding a trick to force the refresh

--- GamesClasificados/ClientApp/src/components/ad/ad.component.ts
+++ GamesClasificados/ClientApp/src/components/ad/ad.component.ts
@@ -1,4 +1,4 @@
-﻿import { Component, OnInit, OnDestroy, Input, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, EventEmitter } from '@angular/core';
 import { MatDialogRef, MatDialog } from '@angular/material';
 import { Ad } from '../../models/ad';
 import { User } from '../../models/user'
@@ -12,79 +12,81 @@ import { ConsoleService } from '../../services/console.service';
 import { YesCancelDialogComponent } from '../dialogyescancel/yes-cancel-dialog.component';
 
 @Component({
-    selector: 'app-ad',
-    templateUrl: './ad.component.html',
-    styleUrls: ['./ad.component.css']
+  selector: 'app-ad',
+  templateUrl: './ad.component.html',
+  styleUrls: ['./ad.component.css']
 })
 export class AdComponent implements OnInit, OnDestroy {
-    @Input()
-    ad: Ad;
-    @Input()
-    adEmitter: EventEmitter<Ad>;
-    userid: number = -1;
-    sections: IHashMap;
-    consoles: IHashMap;
+  @Input()
+  ad: Ad;
+  @Input()
+  adEmitter: EventEmitter<Ad>;
+  userid: number = -1;
+  sections: IHashMap;
+  consoles: IHashMap;
 
-    textDecoration: string = '';
+  textDecoration: string = '';
 
-    constructor(
-        private matDialog: MatDialog,
-        private authService: AuthService,
-        private adService: AdService,
-        private sectionService: SectionService,
-        private consoleService: ConsoleService) { }
+  constructor(
+    private matDialog: MatDialog,
+    private authService: AuthService,
+    private adService: AdService,
+    private sectionService: SectionService,
+    private consoleService: ConsoleService) { }
 
-    ngOnInit(): void {
-        this.setConsolesSections();
-        if (this.authService.isUserLoggedIn()) {
-            this.userid = this.authService.user.id;
-        }
-        this.setTextDecorator();
-        // Subscribe for update if needed
-        if (this.adEmitter) {
-            this.adEmitter.subscribe(
-                (ad: Ad) => {
-                    this.ad = ad;
-                });
-        }
+  ngOnInit(): void {
+    this.setConsolesSections();
+    if (this.authService.isUserLoggedIn()) {
+      this.userid = this.authService.user.id;
     }
-
-    ngOnDestroy(): void {
-        if (this.adEmitter) {
-            this.adEmitter.unsubscribe();
-        }
-    }
-
-    deleteAd(ad: Ad) {
-        let dialogRef: MatDialogRef<YesCancelDialogComponent> = this.matDialog.open(YesCancelDialogComponent, {
-            disableClose: false
-        });
-        dialogRef.componentInstance.message = 'Are you sure you want to delete your ad?';
-        dialogRef.afterClosed().subscribe((result: any) => {
-            if (result['response'] === true) {
-                this.adService.deleteAd(this.ad.id).then((deleted) => {
-                    this.ad.expired = deleted;
-                    this.setTextDecorator();
-                });
-            }
+    this.setTextDecorator();
+    // Subscribe for update if needed
+    if (this.adEmitter) {
+      this.adEmitter.subscribe(
+        (ad: Ad) => {
+          this.ad = ad;
+          let d = new Date();
+          this.ad.imageUrl = this.ad.imageUrl + '?' + d.getTime();
         });
     }
+  }
 
-    // Private Methods
-    private setConsolesSections(): void {
-        if (ConsoleService.consoles) {
-            this.consoles = ConsoleService.consoles;
-        } else {
-            this.consoleService.getConsoles().then(res => this.consoles = res);
-        }
-        if (SectionService.sections) {
-            this.sections = SectionService.sections;
-        } else {
-            this.sectionService.getSections().then(res => this.sections = res);
-        }
+  ngOnDestroy(): void {
+    if (this.adEmitter) {
+      this.adEmitter.unsubscribe();
     }
+  }
 
-    private setTextDecorator(): void {
-        this.textDecoration = this.ad.expired == true ? 'line-through' : '';
+  deleteAd(ad: Ad) {
+    let dialogRef: MatDialogRef<YesCancelDialogComponent> = this.matDialog.open(YesCancelDialogComponent, {
+      disableClose: false
+    });
+    dialogRef.componentInstance.message = 'Are you sure you want to delete your ad?';
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result['response'] === true) {
+        this.adService.deleteAd(this.ad.id).then((deleted) => {
+          this.ad.expired = deleted;
+          this.setTextDecorator();
+        });
+      }
+    });
+  }
+
+  // Private Methods
+  private setConsolesSections(): void {
+    if (ConsoleService.consoles) {
+      this.consoles = ConsoleService.consoles;
+    } else {
+      this.consoleService.getConsoles().then(res => this.consoles = res);
     }
+    if (SectionService.sections) {
+      this.sections = SectionService.sections;
+    } else {
+      this.sectionService.getSections().then(res => this.sections = res);
+    }
+  }
+
+  private setTextDecorator(): void {
+    this.textDecoration = this.ad.expired == true ? 'line-through' : '';
+  }
 }
