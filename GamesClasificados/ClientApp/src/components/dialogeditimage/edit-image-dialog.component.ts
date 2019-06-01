98f8be3149cbe13e70cc861fed528d196ca42c23
Hector Olan <olanhector@gmail.com>
2019-06-01 14:20:19 -0400

    Add copper.js to manage image prior upload it to Azure

--- GamesClasificados/ClientApp/src/components/dialogeditimage/edit-image-dialog.component.ts
+++ GamesClasificados/ClientApp/src/components/dialogeditimage/edit-image-dialog.component.ts
@@ -3,6 +3,9 @@ import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import Cropper from 'cropperjs';
 
+declare var jquery: any;
+declare var $: any;
+
 @Component({
   selector: 'app-edit-image-dialog',
   templateUrl: './edit-image-dialog.component.html',
@@ -26,12 +29,31 @@ export class EditImageDialogComponent implements OnInit, AfterViewInit  {
     let image: HTMLImageElement = <HTMLImageElement> document.getElementById('image');
     this.cropperImage = new Cropper(image, {
       aspectRatio: 1,
-      viewMode: 3,
+      viewMode: 0,
     });
   }
 
-  closeSubmit(response: any) {
-    this.dialogRef.close({ 'response': null });
+  closeSubmit(response: boolean) {
+    if (this.cropperImage) {
+      let canvas = this.cropperImage.getCroppedCanvas({
+        width: 275,
+        height: 275,
+      });
+      let imageBase64 = canvas.toDataURL('image/jpeg');
+      this.dialogRef.close({ 'response': response, 'imageBase64': imageBase64 });
+    }
+  }
+
+  rotate(degree: number) {
+    if (this.cropperImage) {
+      this.cropperImage.rotate(degree);
+    }
+  }
+
+  zoom(zoom: number) {
+    if (this.cropperImage) {
+      this.cropperImage.zoom(zoom);
+    }
   }
 
 }
